05ffa9446171dbf255baf2a67f3f2678741ba61a
Thomas Windt <luigi3654@gmail.com>
2018-10-01 18:33:42 +0200

    Update index.js
    Added a party score command

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ client.on('message', async (msg) => {
   const command = args.shift().slice(prefix.length);
   //Add your commands here. Good command handlers are overrated :POGGERS:
 
-  if(command === 'ping'){
+  if(command === 'partyscore' || command === 'ps'){
+    const m = await msg.channel.send('Calulating party score...');
+    let score = Math.floor(Math.random()*100);
+    m.edit(`You have a party score of \`${score}\`%! Keep on partying!`);
+  } else if(command === 'ping'){
     const m = await msg.channel.send('Pinging...');
     m.edit(`Ponggers! Client ping: \`${m.createdTimestamp - msg.createdTimestamp}\`ms. Heartbeat ping: \`${client.ping}\`ms`);
   } else if(command === 'hello') {
